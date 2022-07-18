4d3ca0e06db050a446699a629dbbcce3b9dfec86
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2022-07-18 08:20:59 -0700

    Add theme two JS object

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const themeButtons = document.querySelectorAll('.select__btn');
 let root = document.documentElement;
 
 themeButtons[0].addEventListener('click', () => {
-    console.log("Boogie Woggie!!!!");
-    root.style.setProperty('--main-background', '#fff');
+    // console.log("Boogie Woggie!!!!");
+    // root.style.setProperty('--main-background', '#fff');
+    changeThemeTo(themes.themeTwo);
 })
 
 function changeThemeTo(theme) {
@@ -34,27 +35,25 @@ const themes = {
         "--equal-key-box-shadow":  `#93261A`,
         "--equal-key-hover-color": `#F96B5B`, // ALSO THE TOGGLE BUTTON COLOR
     },
-
     themeTwo: {
-        "--primary-color":     `#ffffff`,
-        "--main-background":   `#3A4663`,
-        "--screen-background": `#181F33`,
-        "--toggle-background": `#242D44`,
-        "--keypad-background": `#242D44`,
-        "--primary-keys-background":  `#EAE3DC`,
-        "--primary-keys-text-color": ` #434A59`,
-        "--primary-keys-box-shadow":  `#B3A497`,
+        "--primary-color":     `#36362C`,
+        "--main-background":   `#E6E6E6`,
+        "--screen-background": `#EEEEEE`,
+        "--toggle-background": `#D2CDCD`,
+        "--keypad-background": `#D2CDCD`,
+        "--primary-keys-background":  `#E5E4E1`,
+        "--primary-keys-text-color": ` #36362C`,
+        "--primary-keys-box-shadow":  `#A79E91`,
         "--primary-keys-hover-color": `#ffffff`,
-        "--secondary-keys-background":  `#647198`,
+        "--secondary-keys-background":  `#378187`,
         "--secondary-keys-text-color":  `#FFFFFF`,
-        "--secondary-keys-box-shadow":  `#414E73`,
-        "--secondary-keys-hover-color": `#A2B2E1`,
-        "--equal-key-background":  `#D03F2F`, // ALSO THE TOGGLE BUTTON COLOR
+        "--secondary-keys-box-shadow":  `#1B6066`,
+        "--secondary-keys-hover-color": `#62B5BC`,
+        "--equal-key-background":  `#C85402`, // ALSO THE TOGGLE BUTTON COLOR
         "--equal-key-text-color":  `#FFFFFF`,
-        "--equal-key-box-shadow":  `#93261A`,
-        "--equal-key-hover-color": `#F96B5B`, // ALSO THE TOGGLE BUTTON COLOR 
+        "--equal-key-box-shadow":  `#873901`,
+        "--equal-key-hover-color": `#FF8A38`, // ALSO THE TOGGLE BUTTON COLOR 
     },
-
     themeThree: {
         "--primary-color":     `#ffffff`,
         "--main-background":   `#3A4663`,
@@ -75,5 +74,3 @@ const themes = {
         "--equal-key-hover-color": `#F96B5B`, // ALSO THE TOGGLE BUTTON COLOR
     },
 }
-
-// changeThemeTo(themes.themeTwo);
