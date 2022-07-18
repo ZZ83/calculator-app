6626ce309f456b0c079996456d4ae6e288780e05
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2022-07-18 08:47:59 -0700

    Add theme three JS object

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let root = document.documentElement;
 themeButtons[0].addEventListener('click', () => {
     // console.log("Boogie Woggie!!!!");
     // root.style.setProperty('--main-background', '#fff');
-    changeThemeTo(themes.themeTwo);
+    changeThemeTo(themes.themeThree);
 })
 
 function changeThemeTo(theme) {
@@ -55,22 +55,23 @@ const themes = {
         "--equal-key-hover-color": `#FF8A38`, // ALSO THE TOGGLE BUTTON COLOR 
     },
     themeThree: {
-        "--primary-color":     `#ffffff`,
-        "--main-background":   `#3A4663`,
-        "--screen-background": `#181F33`,
-        "--toggle-background": `#242D44`,
-        "--keypad-background": `#242D44`,
-        "--primary-keys-background":  `#EAE3DC`,
-        "--primary-keys-text-color": ` #434A59`,
-        "--primary-keys-box-shadow":  `#B3A497`,
-        "--primary-keys-hover-color": `#ffffff`,
-        "--secondary-keys-background":  `#647198`,
+        "--primary-color":     `#FFE53D`,
+        "--main-background":   `#17062A`,
+        "--screen-background": `#1E0936`,
+        "--toggle-background": `#1E0936`,
+        "--keypad-background": `#1E0936`,
+        "--primary-keys-background":  `#331C4D`,
+        "--primary-keys-text-color": ` #FFE53D`,
+        "--primary-keys-box-shadow":  `#881C9E`,
+        "--primary-keys-hover-color": `#6C34AC`,
+        "--secondary-keys-background":  `#56077C`,
         "--secondary-keys-text-color":  `#FFFFFF`,
-        "--secondary-keys-box-shadow":  `#414E73`,
-        "--secondary-keys-hover-color": `#A2B2E1`,
-        "--equal-key-background":  `#D03F2F`, // ALSO THE TOGGLE BUTTON COLOR
-        "--equal-key-text-color":  `#FFFFFF`,
-        "--equal-key-box-shadow":  `#93261A`,
-        "--equal-key-hover-color": `#F96B5B`, // ALSO THE TOGGLE BUTTON COLOR
+        "--secondary-keys-box-shadow":  `#BE15F4`,
+        "--secondary-keys-hover-color": `#8631AF`,
+        "--equal-key-background":  `#00DED0`, // ALSO THE TOGGLE BUTTON COLOR
+        "--equal-key-text-color":  `#1A2327`,
+        "--equal-key-box-shadow":  `#6CF9F1`,
+        "--equal-key-hover-color": `#93FFF8`, // ALSO THE TOGGLE BUTTON COLOR
     },
 }
+
